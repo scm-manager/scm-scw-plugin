4219334cb38c8ea1097969ef7f6fda0cf78c31ab
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-04-29 13:15:53 +0200

    implement custom markdown link protocol renderer to allow external video content (#2)
    A new core change allows developers to implement renderers for custom link protocols in markdown. This pull request introduces the scw protocol which transforms a link into an iframe-embedded video. This properly works with sanitization in contrast to the previous implementation.
    
    Co-authored-by: Eduard Heimbuch <eduard.heimbuch@cloudogu.com>

--- src/main/js/ScwLinkRenderer.tsx
+++ src/main/js/ScwLinkRenderer.tsx
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2020-present Cloudogu GmbH and Contributors
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,21 +21,20 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import React, { FC } from "react";
 
+const ScwLinkRenderer: FC<{ href: string }> = ({ href, children }) => {
+  return (
+    <iframe
+      width={300}
+      height={169}
+      src={href}
+      title={children?.toString()}
+      frameBorder={0}
+      allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen={true}
+    />
+  );
+};
 
-plugins {
-  id 'org.scm-manager.smp' version '0.8.0'
-}
-
-dependencies {
-  plugin "sonia.scm.plugins:scm-review-plugin:2.9.0"
-}
-
-scmPlugin {
-  scmVersion = "2.17.2-SNAPSHOT"
-  displayName = "Secure Core Warrior"
-  description = "Integrates Secure Code Warrior learning material for related security issues."
-  author = "Cloudogu GmbH"
-  category = "Information"
-
-}
+export default ScwLinkRenderer;
